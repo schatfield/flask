376e32aa065ac557b4d58b09c9e1dbbf59935915
Schatfield <dgoldmine@icloud.com>
2020-09-21 17:22:15 -0500

    created model and api actions view file

---
+++ note_model.py
@@ -0,0 +1,12 @@
+class NoteModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.String(255))
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+class NoteSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = NoteModel
